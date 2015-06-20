75cbcd5b5b1b077602a24f29036e46b70eb4fd76
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-06-20 15:03:58 +0100

    Modular code, conjugate gradient method

--- docs/input/LinearRegression.fsx
+++ docs/input/LinearRegression.fsx
@@ -32,7 +32,10 @@ let mutable w = Rnd.Vector(data.X.Rows)
 
 w <- Train {DefaultParams with Epochs = 150; Batch = Minibatch 3} train h w
 
-w <- Train {DefaultParams with Epochs = 10; Batch = Full; OptimizeFunction = Optimize.Newton} train h w
+w <- Train {DefaultParams with Epochs = 10; Batch = Full; OptimizeMethod = GD; Momentum = None; ReportInterval = 1} train h w
+
+
+w <- Train {DefaultParams with Epochs = 130; Batch = Full; OptimizeMethod = Newton; Momentum = None} train h w
 
 let model = h w
 
--- src/Hype/Optimize.fs
+++ src/Hype/Optimize.fs
@@ -47,104 +47,22 @@ type Batch =
     | Minibatch of int
     | Stochastic // Minibatch with size 1
 
-type Params =
+type OptimizeMethod =
+    | GD
+    | NonlinearCGD
+    | Newton
+    | Custom of (Params->(Vector<D>->D)->Vector<D>->Vector<D>)
+
+and Params =
     {Epochs : int
      LearningRate : LearningRate
      Momentum : Momentum
      LossFunction : Data->(Vector<D>->Vector<D>)->D
-     OptimizeFunction: Params->(Vector<D>->D)->Vector<D>->Vector<D>
+     OptimizeMethod: OptimizeMethod
      Batch : Batch
      Verbose : bool
      ReportFunction : int->Vector<D>->D->unit
      ReportInterval : int}
-
-type Optimize =
-    static member GD (par:Params) (f:Vector<D>->D) (w0:Vector<D>) =
-        Util.printLog "Method: Gradient descent"
-        Util.printLog (sprintf "Dimensions: %A" w0.Length)
-        let epochs = 
-            match par.LearningRate with
-            | Scheduled l -> l.Length
-            | _ -> par.Epochs
-        Util.printLog (sprintf "Epochs: %A" epochs)
-        let momentum =
-            match par.Momentum with
-            | Momentum m -> 
-                Util.printLog (sprintf "Momentum: Constant %A" m)
-                fun (uprev:Vector<D>) (u:Vector<D>) -> m * uprev + (D 1. - m) * u
-            | None -> 
-                Util.printLog "Momentum: none"
-                fun _ u -> u
-        let update =
-            match par.LearningRate with
-            | Constant l -> 
-                Util.printLog (sprintf "Learning rate: Constant %A" l)
-                fun _ w f -> 
-                    let v, g = grad' f w    
-                    let w' = -l * g
-                    v, w'
-            | Decreasing (l0, t) ->
-                Util.printLog (sprintf "Learning rate: Decreasing %A %A" l0 t)
-                fun i w f -> 
-                    let v, g = grad' f w
-                    let w' = -l0 * t  * g / (t + float i)
-                    v, w'
-            | Scheduled l -> 
-                Util.printLog (sprintf "Learning rate: Scheduled of length %A" l.Length)
-                fun i w f -> 
-                    let v, g = grad' f w
-                    let w' = -l.[i] * g
-                    v, w'
-            | Backtracking (a, b) ->
-                Util.printLog (sprintf "Learning rate: Backtracking %A %A" a b)
-                fun _ w f -> 
-                    let v, g = grad' f w
-                    let gg = Vector.normSq g
-                    let mutable l = D 1.
-                    while f (w - l * g) > v - a * l * gg do
-                        l <- l * b
-                    let w' = -l * g
-                    v, w'
-        let mutable i = 0
-        let mutable w = Vector.copy w0
-        let mutable v = f w0
-        let mutable u = Vector.create w.Length (D 0.)
-        if par.Verbose then 
-            Util.printLog (sprintf "Epoch %A function value: %A" i (primal v))
-        par.ReportFunction i w v
-        while i < epochs do
-            let v', u' = update i w f
-            u <- momentum u u'
-            w <- w + u
-            v <- v'
-            i <- i + 1
-            if i % par.ReportInterval = 0 then 
-                if par.Verbose then 
-                    Util.printLog (sprintf "Epoch %A function value: %A" i (primal v))
-                par.ReportFunction i w v
-        w
-
-    static member Newton (par:Params) (f:Vector<D>->D) (w0:Vector<D>) =
-        Util.printLog "Method: Exact Newton"
-        Util.printLog (sprintf "Dimensions: %A" w0.Length)
-        Util.printLog (sprintf "Epochs: %A" par.Epochs)
-        let mutable i = 0
-        let mutable w = Vector.copy w0
-        let mutable v = f w0
-        par.ReportFunction i w v
-        if par.Verbose then 
-            Util.printLog (sprintf "Epoch %A function value: %A" i (primal v))
-        while i < par.Epochs do
-            let v', g, h = gradhessian' f w
-            let p = - Matrix.solve h g
-            w <- w + p
-            v <- primal v'
-            i <- i + 1
-            if i % par.ReportInterval = 0 then
-                if par.Verbose then 
-                    Util.printLog (sprintf "Epoch %A function value: %A" i (primal v))
-                par.ReportFunction i w v
-        w
 
 
 [<AutoOpen>]
@@ -153,11 +71,90 @@ module Ops =
                          LearningRate = Backtracking (D 0.25, D 0.75)
                          Momentum = Momentum (D 0.5)
                          LossFunction = Loss.Quadratic
-                         OptimizeFunction = Optimize.GD
+                         OptimizeMethod = GD
                          Batch = Minibatch 3
                          Verbose = true
                          ReportFunction = fun _ _ _ -> ()
                          ReportInterval = 10}
+
+
+    let Optimize (par:Params) (f:Vector<D>->D) (w0:Vector<D>) =
+        match par.OptimizeMethod with
+        | Custom c -> c par f w0
+        | _ ->
+            let dir =
+                match par.OptimizeMethod with
+                | GD ->
+                    Util.printLog "Method: Gradient descent"
+                    fun _ w f _ _ _ ->
+                        let v', g' = grad' f w
+                        v', g', -g'
+                | NonlinearCGD -> // Fletcher and Reeves 1964
+                    Util.printLog "Method: Nonlinear conjugate gradient"
+                    fun i w f _ g p ->
+                        let v', g' = grad' f w
+                        let b = (Vector.normSq g') / (Vector.normSq g)
+                        let p' = -g' + b * p
+                        v', g', p'
+                | Newton ->
+                    Util.printLog "Method: Exact Newton"
+                    fun _ w f _ g _ ->
+                        let v', g', h' = gradhessian' f w
+                        v', g', - Matrix.solve h' g'
+            Util.printLog (sprintf "Dimensions: %A" w0.Length)
+            let epochs = 
+                match par.LearningRate with
+                | Scheduled l -> l.Length
+                | _ -> par.Epochs
+            Util.printLog (sprintf "Epochs: %A" epochs)
+            let lr =
+                match par.LearningRate with
+                | Constant l ->
+                    Util.printLog (sprintf "Learning rate: Constant %A" l)
+                    fun _ _ _ _ _ -> l
+                | Decreasing (l0, t) ->
+                    Util.printLog (sprintf "Learning rate: Decreasing %A %A" l0 t)
+                    fun i _ _ _ _ -> l0 * t / (t + float i)
+                | Scheduled l ->
+                    Util.printLog (sprintf "Learning rate: Scheduled of length %A" l.Length)
+                    fun i _ _ _ _ -> l.[i]
+                | Backtracking (a, b) ->
+                    Util.printLog (sprintf "Learning rate: Backtracking %A %A" a b)
+                    fun i w f v g ->
+                        let gg = Vector.normSq g
+                        let mutable l = D 1.
+                        while f (w - l * g) > v - a * l * gg do
+                            l <- l * b
+                        l
+            let momentum =
+                match par.Momentum with
+                | Momentum m ->
+                    Util.printLog (sprintf "Momentum: Constant %A" m)
+                    fun (p:Vector<D>) (p':Vector<D>) -> m * p + (D 1. - m) * p'
+                | None ->
+                    Util.printLog "Momentum: None"
+                    fun _ p' -> p'
+            let mutable i = 0
+            let mutable w = Vector.copy w0
+            let v, g = grad' f w0
+            let mutable v = v
+            let mutable g = g
+            let mutable p = -g
+            par.ReportFunction i w v
+            if par.Verbose then
+                Util.printLog (sprintf "Epoch %A function value: %A" i (primal v))
+            while i < epochs do
+                let v', g', p' = dir i w f v g p
+                v <- v'
+                g <- g'
+                p <- momentum p p'
+                w <- w + (lr i w f v g) * p
+                i <- i + 1
+                if i % par.ReportInterval = 0 then
+                    if par.Verbose then
+                        Util.printLog (sprintf "Epoch %A function value: %A" i (primal v))
+                    par.ReportFunction i w v
+            w
 
     let Train (par:Params) (d:Data) (f:Vector<D>->Vector<D>->Vector<D>) (w0:Vector<D>) =
         Util.printLog "Training started."
@@ -172,14 +169,14 @@ module Ops =
                 Util.printLog (sprintf "Batch: Minibatches of %A" n)
                 fun w -> par.LossFunction (d.Minibatch n) (f w)
             | Stochastic ->
-                Util.printLog "Batch: Stochastic"
+                Util.printLog "Batch: Stochastic (minibatch of 1)"
                 fun w -> par.LossFunction (d.Minibatch 1) (f w)
         let report i w v =
             if par.Verbose then
                 let loss = par.LossFunction d (f (w |> Vector.map primal))
                 Util.printLog (sprintf "Epoch %A training loss: %A" i (primal loss))
             par.ReportFunction i w v
-        let wopt = par.OptimizeFunction {par with ReportFunction = report; Verbose = false} q w0
+        let wopt = Optimize {par with ReportFunction = report; Verbose = false} q w0
         let duration = System.DateTime.Now.Subtract(start)
         let lf = par.LossFunction d (f wopt)
         let dec = -(lf - l0)
