68ef80ca1fa772b01447dc2e7510ed05420d882a
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-11-13 01:35:19 +0000

    Cleanup

--- docs/input/RecurrentNets.fsx
+++ docs/input/RecurrentNets.fsx
@@ -1,8 +1,11 @@
-﻿
+﻿(*** hide ***)
 #r "../../src/Hype/bin/Release/DiffSharp.dll"
 #r "../../src/Hype/bin/Release/Hype.dll"
+#I "../../packages/R.NET.Community.1.6.4/lib/net40/"
+#I "../../packages/R.NET.Community.FSharp.1.6.4/lib/net40/"
 #I "../../packages/RProvider.1.1.14"
 #load "RProvider.fsx"
+fsi.ShowDeclarationValues <- true
 
 open Hype
 open Hype.Neural
