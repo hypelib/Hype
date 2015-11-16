d9a9268e256fa84f51cf5734f3160f970c5e03ee
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-11-16 00:38:12 +0000

    MCMC

--- docs/input/HMC.fsx
+++ docs/input/HMC.fsx
@@ -1,33 +1,15 @@
-﻿
+﻿(*** hide ***)
 #r "../../src/Hype/bin/Release/DiffSharp.dll"
 #r "../../src/Hype/bin/Release/Hype.dll"
+#I "../../packages/R.NET.Community.1.6.4/lib/net40/"
+#I "../../packages/R.NET.Community.FSharp.1.6.4/lib/net40/"
 #I "../../packages/RProvider.1.1.14"
 #load "RProvider.fsx"
+fsi.ShowDeclarationValues <- false
 
-open Hype
-open Hype.Neural
-open DiffSharp.AD.Float32
-open DiffSharp.Util
+(**
+Markov Chain Monte Carlo
+========================
 
-open RDotNet
-open RProvider
-open RProvider.graphics
-
-
-
-
-let dataor = {X = toDM [[0.; 0.; 1.; 1.]
-                        [0.; 1.; 0.; 1.]]
-              Y = toDM [[0.; 1.; 1.; 1.]]}
-
-
-
-let n = FeedForward()
-n.Add(Linear(2, 4))
-n.Add(Activation(sigmoid))
-n.Add(Linear(4, 1))
-n.Add(Activation(sigmoid))
-
-n.ToStringFull() |> printfn "%s"
-
-Layer.Train(n, dataor)
+Documentation coming soon.
+*)
---
+++ src/Hype/Inference.fs
@@ -0,0 +1,52 @@
+﻿//
+// This file is part of
+// Hype: Compositional Machine Learning and Hyperparameter Optimization
+//
+// Copyright (c) 2015, National University of Ireland Maynooth (Atilim Gunes Baydin, Barak A. Pearlmutter)
+//
+// Hype is released under the MIT license.
+// (See accompanying LICENSE file.)
+//
+// Written by:
+//
+//   Atilim Gunes Baydin
+//   atilimgunes.baydin@nuim.ie
+//
+//   Barak A. Pearlmutter
+//   barak@cs.nuim.ie
+//
+//   Brain and Computation Lab
+//   Hamilton Institute & Department of Computer Science
+//   National University of Ireland Maynooth
+//   Maynooth, Co. Kildare
+//   Ireland
+//
+//   www.bcl.hamilton.ie
+//
+
+namespace Hype.Inference
+
+open Hype
+open DiffSharp.AD.Float32
+open DiffSharp.Util
+
+type HMCSampler() =
+
+    static member Sample(n, hdelta, hsteps, x0:DV, f:DV->D) =
+        let leapFrog (u:DV->D) (k:DV->D) (d:D) steps (x0, p0) =
+            let hd = d / 2.f
+            [1..steps] 
+            |> List.fold (fun (x, p) _ ->
+                let p' = p - hd * grad u x
+                let x' = x + d * grad k p'
+                x', p' - hd * grad u x') (x0, p0)
+
+        let u x = -log (f x) // potential energy
+        let k p = (p * p) / D 2.f // kinetic energy
+        let hamilton x p = u x + k p
+        let x = ref x0
+        [|for i in 1..n do
+            let p = DV.init x0.Length (fun _ -> Rnd.Normal())
+            let x', p' = leapFrog u k hdelta hsteps (!x, p)
+            if Rnd.Uniform() < float32 (exp ((hamilton !x p) - (hamilton x' p'))) then x := x'
+            yield !x|]
