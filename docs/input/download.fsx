3267509709be916a25bbf7af239b8d6ed03ca051
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-12-16 17:31:01 +0000

    Minor

--- docs/input/download.fsx
+++ docs/input/download.fsx
@@ -8,5 +8,5 @@ You can download the source code or the binaries of the [latest release on GitHu
 
 You can also install the library as a package through [NuGet](https://www.nuget.org/packages/Hype), by running <pre>Install-Package Hype</pre> in the package manager console.
 
-Hype only supports the 64 bit platform, so please make sure that you set "x64" as the platform target. For detailed instructions, please see the [installation instructions](http://diffsharp.github.io/DiffSharp/download.html) of DiffSharp, on which Hype depends.
+Hype only supports the 64 bit platform, so please make sure that you set "x64" as the platform target. For detailed instructions, please see the installation instructions of [DiffSharp](http://diffsharp.github.io/DiffSharp/), on which Hype depends.
 *)
