17ddae76aedc2b574a8dab69cecf384aab77db8d
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-12-26 00:05:11 +0000

    Bump version

--- src/Hype/AssemblyInfo.fs
+++ src/Hype/AssemblyInfo.fs
@@ -34,9 +34,9 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("0.1.2")>]
-[<assembly: AssemblyInformationalVersion("0.1.2")>]
-[<assembly: AssemblyFileVersion("0.1.2.*")>]
+[<assembly: AssemblyVersion("0.1.3")>]
+[<assembly: AssemblyInformationalVersion("0.1.3")>]
+[<assembly: AssemblyFileVersion("0.1.3.*")>]
 
 do
     ()
